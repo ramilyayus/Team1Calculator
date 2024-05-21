8dbad983b583daef0e26310aa5f2c002404c1766
Yusupova Ramilya <yusupova@mindbox.cloud>
2024-05-21 10:35:48 +0300

    Add single product quantity processing

--- Team1Calculator/Program.cs
+++ Team1Calculator/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+Console.WriteLine($"Введите цену:");
+var price = Convert.ToDouble(Console.ReadLine());
+var quantity = 1;
+
+var calculator = new Calculator();
+var result = calculator.Calculate(new Product(price, quantity));
+Console.WriteLine($"Итоговая стоимость: {result}");
+
+public record Product(double Price, int Quantity);
+public class Calculator
+{
+    public double Calculate(Product product)
+    {
+        return product.Quantity * product.Price;
+    }
+}
