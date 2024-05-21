aa304a6d2003bbc9e82b2bd4938dcfce2157270a
Ekaterina Grishina <grishina@mindbox.cloud>
2024-05-21 11:45:24 +0400

    Calculate sum for single state

--- Team1Calculator/Program.cs
+++ Team1Calculator/Program.cs
@@ -4,16 +4,19 @@ Console.WriteLine($"Введите цену:");
 var price = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine($"Введите количество товара:");
 var quantity = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Введите штат:");
+var state = Console.ReadLine();
 
 var calculator = new Calculator();
-var result = calculator.Calculate(new Product(price, quantity));
+var result = calculator.Calculate(new Product(price, quantity), state);
 Console.WriteLine($"Итоговая стоимость: {result}");
 
 public record Product(double Price, int Quantity);
 public class Calculator
 {
-    public double Calculate(Product product)
+    public double Calculate(Product product, string state)
     {
-        return product.Quantity * product.Price;
+        var sum = product.Quantity * product.Price;
+        return state == "UT" ? (sum * 1.0685) : sum;
     }
 }
