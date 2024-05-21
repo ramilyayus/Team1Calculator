f8025029b535513103e81b61629c6cc78f03ecba
Ekaterina Grishina <grishina@mindbox.cloud>
2024-05-21 12:24:39 +0400

    Calculate sum for all discounts

--- Team1Calculator/Program.cs
+++ Team1Calculator/Program.cs
@@ -7,9 +7,6 @@ var quantity = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine($"Введите штат:");
 var state = Console.ReadLine();
 
-var saleTrashhold = 1000;
-var salePersentage = 0.03;
-
 var allStates = StatesRepository.GetAllStates();
 var stateEntity = allStates.Single(x => x.Code == state);
 
@@ -17,18 +14,14 @@ var calculator = new Calculator();
 var result = calculator.Calculate(new Product(price, quantity), stateEntity);
 
 var saleCalculator = new SaleCalculator();
-if (result >= saleTrashhold)
-{
-    var resultWithSale = saleCalculator.Calculate(sum: result, sale: salePersentage);
-    Console.WriteLine($"Итоговая стоимость: {resultWithSale}");
-}
-else
-{
-    Console.WriteLine($"Итоговая стоимость: {result}");
-}
+var allDiscounts = DiscountRepository.GetAllDiscounts();
+var discountRange = allDiscounts.Single(r => r.LeftBound <= result && result <= r.RightBound);
+var resultWithSale = saleCalculator.Calculate(sum: result, sale: discountRange.Value);
+Console.WriteLine($"Итоговая стоимость: {resultWithSale}");
 
 public record Product(double Price, int Quantity);
 public record State(string Code, double Tax);
+public record Discount(double LeftBound, double RightBound, double Value);
 
 public class SaleCalculator
 {
@@ -58,6 +51,22 @@ public static class StatesRepository
             new State("TX", 6.25),
             new State("AL", 4.00),
             new State("CA", 8.25)
+        };
+    }
+}
+
+public static class DiscountRepository
+{
+    public static List<Discount> GetAllDiscounts()
+    {
+        return new List<Discount>()
+        {
+            new Discount(0, 999, 0.00),
+            new Discount(1000, 4999, 0.03),
+            new Discount(5000, 6999, 0.05),
+            new Discount(7000, 9999, 0.07),
+            new Discount(10000, 14999, 0.1),
+            new Discount(15000, double.MaxValue, 0.15),
         };
     }
 }
